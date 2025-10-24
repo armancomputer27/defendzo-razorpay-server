57e27dfffe88eabdf9c09205826d2e402de60026
armancomputer27 <armancomputer27@gmail.com>
2025-10-24 08:30:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -47,4 +47,5 @@ app.post("/create-mandate", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(✅ Server running on port ));
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
